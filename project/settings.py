fa4c3faff1368f6c8be28040595dfbe34f4114e8
saeedmhmoud100 <saeedmhmoud109@gmail.com>
2024-07-01 15:07:03 +0300

    install channels[daphne] and configure some settings

--- project/settings.py
+++ project/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "daphne",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "channels",
+
+    "liveCalculator",
 ]
 
 MIDDLEWARE = [
@@ -121,3 +125,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+ASGI_APPLICATION = "project.asgi.application"
